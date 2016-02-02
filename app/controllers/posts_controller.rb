d595f6e20a2004541e3d3716f112cd76579d8c84
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-01 19:14:57 -0500

    Initial commit with post controller and viewers funtioning

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+
+  before_action :find_by_id, only: [:show, :edit, :update, :destroy ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+
+
+  def update
+
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @post.delete
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :blog_post)
+    end
+
+    def find_by_id
+        @post = Post.find(params[:id])
+    end
+
+
+end
