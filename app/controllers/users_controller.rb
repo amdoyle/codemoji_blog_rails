caff55e0b3637906036c0051d8a9dd35b32150ac
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-01 20:36:18 -0500

    Added user model and session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to posts_path, notice: "You're Signed Up!"
+    else
+      reload :edit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
