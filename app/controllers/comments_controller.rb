215544908f38ea6b3e489d65e334753bec32e11d
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-03 19:13:25 -0500

    Add routes and controller for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,53 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+
+    @comment = Comment.find(params[:id])
+
+  end
+
+  def updated
+
+    @comment = Comment.find(params[:id])
+
+    if comment.update_attributes(comment_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
+  end
+
+  def destory
+
+    @comment = Comment.find(params[:id])
+
+    @comment.destory
+    
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).require(:comment)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:new, :create, :edit, :update :destroy]
+  end
+
   resources :users
+  
   resources :sessions, only: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
