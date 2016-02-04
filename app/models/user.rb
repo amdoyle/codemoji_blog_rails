3f04a6ea4ad086f6c2b23c2b6e85fffc53712a8d
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-03 19:01:05 -0500

    Add flash notice for login and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :posts
+
+  validates :email, confirmation: true
+
+  validates :first_name, :last_name, presence: true, length: {minimum: 3}
+
 end
