c4664909423bc6769aee69fccea3db2e960d6813
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-03 20:16:48 -0500

    Add post foreign key to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201215143) do
+ActiveRecord::Schema.define(version: 20160204011033) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
     t.integer  "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
