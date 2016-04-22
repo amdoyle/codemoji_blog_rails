fded17c3ea7f88689e73e2abd77e69543414bde1
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-04-22 11:45:24 -0400

    Update UserMailer to inherit from devise mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
-class UserMailer < ApplicationMailer
+class UserMailer < Devise::Mailer
+  helper :application
+  include Devise::controller::URLHelpers
+  default template_path: 'devise/mailer'
+  
+  def confirmation_email
 end
