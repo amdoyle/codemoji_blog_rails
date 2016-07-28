771fe959550f5c8d225160e1ae0e18e72bac9c9b
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-07-27 23:07:10 -0400

    Updated migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414030601) do
+ActiveRecord::Schema.define(version: 20160728025946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160414030601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
-    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
